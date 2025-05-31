c72a07dbf93fd87838815cabaeaae8831ada44ad
amiramahmedjan <mahmedjan.amira@gmail.com>
2025-05-31 14:30:33 -0700

    initial commit

---
+++ Al-Faarisa/Al-Faarisa/Al_FaarisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Al_FaarisaApp.swift
+//  Al-Faarisa
+//
+//  Created by Amira Mahmedjan on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Al_FaarisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Al-Faarisa/Al-Faarisa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Al-Faarisa
+//
+//  Created by Amira Mahmedjan on 31.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
